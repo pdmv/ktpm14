034bb564ea5c6c2804d21e437fec4fec9a0f1c59
pdmv <2151013110Vuong@ou.edu.vn>
2023-10-23 14:32:56 +0700

    96 test conflict main

--- helloworld.cpp
+++ helloworld.cpp
@@ -7,7 +7,7 @@ int main()
     cout<<"Hello World";
 
     cout<<"Them code ngay 23/10/2023"; // 96 them code
-    cout<<"Them code ngay 23/10/2023 by CMD"; // 96 them code
+    cout<<"Them code ngay 23/10/2023 by CMD --- Test conflict nhanh main"; // 96 them code
 
 
     return 0;
